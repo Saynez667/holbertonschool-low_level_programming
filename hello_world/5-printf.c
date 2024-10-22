eccd68a23bb4c1b50d82a2594bca5a5e0218e841
Saynez667 <saynezironx@gmail.com>
2024-10-22 15:38:56 +0200

    dsffs

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,10 @@
+gcc -w -Wall
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-{puts("\"with proper grammar, but the outcome is a piece of art,");
+{printf("\"with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
