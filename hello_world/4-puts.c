d9330b82bd1be48d7e5f6d396e27ed7f40194e94
Saynez667 <saynezironx@gmail.com>
2024-10-22 13:55:43 +0200

    fdsv

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+int main()
+{
+    char* str1 = "Programming is like building a multilingual puzzle";
+    puts(str1);
+ 
+    puts("Programming is like building a multilingual puzzle");
+ 
+    return 0;
