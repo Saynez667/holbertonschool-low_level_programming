9b9afa8b92d169e119211211d20e961665d3fc6d
root <saynezironx@gmail.com>
2024-10-24 11:28:25 +0000

    dfsf

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char num;
+for (num = '0' ; num <= '10';num++)
+{
+printf("num");
+}
+putchar('\n');
+return (0);
+}
