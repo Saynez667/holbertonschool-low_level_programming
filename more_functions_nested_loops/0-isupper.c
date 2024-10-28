f631d9ebce938f0a8679bb00bfdb94f587cfbc64
root <saynezironx@gmail.com>
2024-10-28 10:12:09 +0000

    fds

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*/
+int _isupperv(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
