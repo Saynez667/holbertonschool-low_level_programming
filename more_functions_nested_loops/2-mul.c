7bdc41fd29a16c67f2467d3cc8545dfccdc0962a
root <saynezironx@gmail.com>
2024-10-28 11:32:26 +0000

    fsd

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,12 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-/*
+/**
+* mul - Multiplies two integers
+* @a: The first integer
+* @b: The second integer
 *
-**/ 
+* Return: The result of the multiplication
+*/ 
 int mul(int a, int b)
 {
 if (a >= 1 && b >= 2)	
 printf("%d", a * b);
-return (a*b);
+return (a * b);
 }
 
