012c19b6373466ed58357debad7f900d01e0d4c4
root <saynezironx@gmail.com>
2024-10-29 14:26:56 +0000

    dfs

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,17 +9,18 @@ if (size <= 0)
 {
 _putchar('\n');
 return;
-}
-for (int i = 1; i <= size; i++)
-{
-for (int j = size - i; j > 0; j--)
+    }
+int i, j, k;
+for (i = 1; i <= size; i++)
+{        
+for (j = size - i; j > 0; j--)
 {
 _putchar(' ');
 }
-for (int k = 0; k < i; k++)
+for (k = 0; k < i; k++)
 {
 _putchar('#');
 }
 _putchar('\n');
-
+}
 }
