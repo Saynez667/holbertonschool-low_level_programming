5546b05fde4408613d892827e31b33f0e0119337
root <saynezironx@gmail.com>
2024-10-29 11:10:24 +0000

    fds

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* more_numbers - prints numbers from 0 to 14, ten times
+* more_numbers - prints 10 times the numbers, from 0 to 14
 */
 void more_numbers(void)
 {
@@ -9,7 +9,7 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-if (j< > 9)
+if (j > 9)
 _putchar((j / 10) + '0');
 _putchar((j % 10) + '0');
 }
