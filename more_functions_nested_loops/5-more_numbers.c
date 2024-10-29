276325f8f6d1bda5cc12a86f5964a817f5c17afd
root <saynezironx@gmail.com>
2024-10-29 10:29:52 +0000

    fs

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - prints the numbers from 0 to 9, followed by a new line
+*
+* Return: void
+*/
+void more_numbers(void)
+{
+char digit;
+for (digit = '0'; digit <= '14'; digit++)
+{
+_putchar(digit);
+}
+_putchar('\n');
+return (10)
+}
