a38441a19f5e5f75622b8be5416e29370e0b475a
root <saynezironx@gmail.com>
2024-10-29 14:31:13 +0000

    dfs

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -4,23 +4,25 @@
 * @size: the size of the triangle
 */
 void print_triangle(int size)
-int i, j, k;
-}
-if (size <= 0)
 {
-_putchar('\n');
-return;
-}
-for (i = 1; i <= size; i++)
-{        
-for (j = size - i; j > 0; j--)
-{
-_putchar(' ');
-}
-for (k = 0; k < i; k++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
+    if (size <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+
+    int i, j, k;
+
+    for (i = 1; i <= size; i++)
+    {
+        for (j = size - i; j > 0; j--)
+        {
+            _putchar(' ');
+        }
+        for (k = 0; k < i; k++)
+        {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
 }
