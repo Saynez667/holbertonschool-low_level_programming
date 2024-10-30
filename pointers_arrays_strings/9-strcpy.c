da16c2c63308f7d05214af1ed3181337f9982914
root <saynezironx@gmail.com>
2024-10-30 20:39:57 +0000

    fds

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -3,7 +3,7 @@
 * _strcpy - Copies the string pointed to by src to dest
 * @dest: Pointer to the destination buffer
 * @src: Pointer to the source string
-* 
+*
 * Return: Pointer to dest
 */
 char *_strcpy(char *dest, char *src)
