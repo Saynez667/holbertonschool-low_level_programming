a235f70ceef968924546b6930f604d0a0ab0d58e
root <saynezironx@gmail.com>
2024-10-31 08:58:28 +0000

    fsd

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -21,7 +21,7 @@ while (s[i] >= '0' && s[i] <= '9')
 if (result > INT_MAX / 10 ||
 (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
 {
-return (sign == 1) ? INT_MAX : INT_MIN;
+return (sign == 1 ? INT_MAX : INT_MIN);
 }
 result = result * 10 + (s[i] - '0');
 i++;
