c329f6e0ba6865d7b5d66280242d16aafff318b2
root <saynezironx@gmail.com>
2024-10-31 13:43:53 +0000

    fds

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,12 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 && (*s1 == *s2))
+int i = 0;
+while (s1[i] == s2[i])
 {
-s1++;
-s2++;
+if (s1[i] == '\0')
+return (0);
+i++;
 }
-return *(unsigned char *)s1 - *(unsigned char *)s2;
+return (s1[i] - s2[i]);
 }
