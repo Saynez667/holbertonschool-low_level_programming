74fa3db844864198379713b896e802ed2278636b
root <saynezironx@gmail.com>
2024-11-05 12:06:06 +0000

    fdsf

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -1,27 +1,42 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <limits.h>
-void reset_to_98(int *n);
-char *_memset(char *s, char b, unsigned int n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-int _putchar(char c);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-char *_strcat(char *dest, char *src);
-void reverse_array(int *a, int n);
-char *string_toupper(char *str);
-int is_separator(char c);
-char *cap_string(char *str);
-char *leet(char *);
-#endif
+#include "main.h"
+#include <stdio.h>
+/**
+* simple_print_buffer - prints buffer in hexa
+* @buffer: the address of memory to print
+* @size: the size of the memory to print
+*
+* Return: Nothing.
+*/
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+unsigned int i;
+i = 0;
+while (i < size)
+{
+if (i % 10)
+{
+printf(" ");
+}
+if (!(i % 10) && i)
+{
+printf("\n");
+}
+printf("0x%02x", buffer[i]);
+i++;
+}
+priintf("\n");
+}
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char buffer[98] = {0x00};
+simple_print_buffer(buffer, 98);
+_memset(buffer, 0x01, 95);
+printf("-------------------------------------------------\n");
+simple_print_buffer(buffer, 98);    
+return (0);
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <limits.h>
+void reset_to_98(int *n);
+char *_memset(char *s, char b, unsigned int n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+void reverse_array(int *a, int n);
+char *string_toupper(char *str);
+int is_separator(char c);
+char *cap_string(char *str);
+char *leet(char *);
+#endif
