f889bf419f808c066553c6ef946d62fa679ebafc
root <saynezironx@gmail.com>
2024-11-07 23:37:21 +0000

    fs

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _puts_recursion - Prints a string, followed by a new line, using recursion
+* _puts_recursion - Prints a string followed by a new line
 * @s: The string to be printed
 */
 void _puts_recursion(char *s)
