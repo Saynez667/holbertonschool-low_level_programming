048836291ab885073c30eb12341affb9c5f83c3b
root <saynezironx@gmail.com>
2024-11-14 21:51:43 +0000

    fsd

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,5 +1,17 @@
 #include <stdlib.h>
 #include <string.h>
+/**
+* _strdup - Duplique une chaîne de caractèr
+* @str: La chaîne de caract�
+* Description: Cette fonction alloue dynamiquement
+* nouvelle chaîne qui est une copie exac
+* - Alloue de l'espace mémoire avec malloc()
+* - Copie le contenu de la chaîne original
+* - Gère le cas où str est NL
+* - Gère l'échec d'allocation mémo
+* - Un pointeur vers la nouvelle chaîne dupliqu
+*- NULL si str est NULL ou si l'allocation mémo
+*/
 char *_strdup(char *str)
 {
 char *duplicate;
