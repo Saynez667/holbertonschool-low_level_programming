42cb1981ae8172a88d5189d952389968ceb95002
root <saynezironx@gmail.com>
2024-11-15 15:04:49 +0000

    vs

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,20 +1,27 @@
 #include <stdlib.h>
-#include <string.h>
-/*
-* _calloc - Alloue de la mémoire pour un tableau et initialise à zéro.
-* @nmemb: Nombre d'éléments à allouer.
-* @size: Taille de chaque élément en octets.
-*
-* Return: Un pointeur vers la mémoire allouée, ou NULL en cas d'erreur.
-*/
+
+/**
+ * _calloc - Allocates memory for an array and initializes it to zero.
+ * @nmemb: The number of elements in the array.
+ * @size: The size of each element.
+ *
+ * Return: A pointer to the allocated memory, or NULL if nmemb or size is 0,
+ *         or if memory allocation fails.
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *ptr;
-if (nmemb == 0 || size == 0)
-return (NULL);
-ptr = malloc(nmemb * size);
-if (ptr == NULL)
-return (NULL);
-memset(ptr, 0, nmemb * size);
-return (ptr);
+    char *ptr;
+    unsigned int i;
+
+    if (nmemb == 0 || size == 0)
+        return (NULL);
+
+    ptr = malloc(nmemb * size);
+    if (ptr == NULL)
+        return (NULL);
+
+    for (i = 0; i < (nmemb * size); i++)
+        ptr[i] = 0;
+
+    return (ptr);
 }
