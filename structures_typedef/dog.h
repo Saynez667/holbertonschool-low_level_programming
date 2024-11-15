fba94d35b656520385b6f331008551c69fdf2422
root <saynezironx@gmail.com>
2024-11-15 15:12:01 +0000

    sf(

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - Structure to store a dog's basic information
+* @name: Name of the dog (type: char *)
+* @age: Age of the dog (type: float)
+* @owner: Owner of the dog (type: char *)
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
