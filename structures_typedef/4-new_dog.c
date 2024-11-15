9235cb62bb2aefe9fb3bfe448c10be420daef077
root <saynezironx@gmail.com>
2024-11-15 15:33:32 +0000

    fs

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,35 +1,47 @@
 #include <stdlib.h>
-#include "dog.h"
 #include <string.h>
+#include "dog.h"
 /**
-* new_dog - creates a new dog
-* @name: name of the dog
-* @age: age of the dog
-* @owner: owner of the dog
-*
-* Return: pointer to the new dog (dog_t), or NULL if failure
+* new_dog - Crée un nouveau chien
+* @name: Le nom du chien
+* @age: L'âge du chien
+* @owner: Le propriétaire du chien
+* Return: Un pointeur vers la nouvelle structure dog_t, ou NULL si l'éche
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog;
-new_dog = malloc(sizeof(dog_t));
-if (new_dog == NULL)
-return (NULL);
-new_dog->name = malloc(strlen(name) + 1);
-if (new_dog->name == NULL)
-{
-free(new_dog);
-return (NULL);
+    dog_t *new_dog;
+
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+    {
+        return (NULL);
+    }
+
+    new_dog->name = malloc(strlen(name) + 1);
+    if (new_dog->name == NULL)
+    {
+        free(new_dog);
+        return (NULL);
+    }
+
+    new_dog->owner = malloc(strlen(owner) + 1);
+    if (new_dog->owner == NULL)
+    {
+        free(new_dog->name);
+        free(new_dog);
+        return (NULL);
+    }
+
+    strcpy(new_dog->name, name);
+    new_dog->age = age;
+    strcpy(new_dog->owner, owner);
+
+    return (new_dog);
 }
-strcpy(new_dog->name, name);
-new_dog->owner = malloc(strlen(owner) + 1);
-if (new_dog->owner == NULL)
-{
-free(new_dog->name);
-free(new_dog);
-return (NULL);
-}
-strcpy(new_dog->owner, owner);
-new_dog->age = age;
-return (new_dog);
-}
+
+
+
+
+
+
