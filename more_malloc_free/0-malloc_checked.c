1a9c14a08d46ae1b89f81959111e0aaf0a41fe9b
root <saynezironx@gmail.com>
2024-11-15 14:54:46 +0000

    vs

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,18 @@
 #include <stdlib.h>
+
 /**
-* malloc_checked - Alloue de la mémoire de taille spécifi
-* @b: La taille en octets de la mémoire à alloue
-* Si malloc() échoue, le programme se termine avec le code 98
-* réussit, un pointeur vers la mémoire allouée est retou
-* Return: Un pointeur vers l'espace mémoire alloué, ou termi
-*/
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to allocate.
+ *
+ * Return: A pointer to the allocated memory.
+ *         If malloc fails, the function causes process termination with status 98.
+ */
 void *malloc_checked(unsigned int b)
 {
-void *ptr = malloc(b);
-{
-exit(98);
-}
-return (ptr);
+    void *ptr = malloc(b);
+
+    if (ptr == NULL)
+        exit(98);
+
+    return (ptr);
 }
