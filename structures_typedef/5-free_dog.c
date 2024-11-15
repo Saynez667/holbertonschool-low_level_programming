cdfa36dc6ccde4dffffc48e8db66cc7bf781540c
root <saynezironx@gmail.com>
2024-11-15 15:28:22 +0000

    fs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+* free_dog - Libère la mémoire allouée pour un chien
+* @d: Pointeur vers la structure dog à libérer
+*/
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
