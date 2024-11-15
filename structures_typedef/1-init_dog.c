9259e3209db24112065c2fd87f8e196525a720c9
root <saynezironx@gmail.com>
2024-11-15 15:17:09 +0000

    gs

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+* init_dog - Initializes a variable of type struct dog
+* @d: Pointer to struct dog to initialize
+* @name: Pointer to the name of the dog
+* @age: Age of the dog
+* @owner: Pointer to the owner's name
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
