4c8cd8070d2ffc9fbd8e54b2b600bdfb163fcbf2
root <saynezironx@gmail.com>
2024-11-20 13:55:10 +0000

    gs

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,4 +1,17 @@
 #include <stddef.h>
+
+/**
+* int_index - Recherche un élément dans un tableau selon un critère spécif
+* @array: Le tableau d'entiers à parcouri
+* @size: Le nombre d'éléments dans le table
+* @cmp: Un pointeur vers une fonction de comparaison
+* Description:
+* - Parcourt le tableau et cherche le premier élément pour lequel
+*la fonction de comparaison ne retourne pas
+*Return:
+*- L'index du premier élément satisfaisant le critère
+*- -1 si aucun élément ne correspond ou en cas d'erreur
+*/
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
