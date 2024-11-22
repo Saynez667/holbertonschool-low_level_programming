d0f728ab37c1b860c421ae81aaad14fcda910fbc
Saynez667 <137231111+Saynez667@users.noreply.github.com>
2024-11-22 09:43:55 +0100

    DSQ

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,16 +1,16 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
-* sum_them_all - Sums all its parameters.
-* @n: The number of parameters.
+*Sum_them_all - Sums all its parameters.
+*@n: The number of parameters.
 *
-* Return: The sum of all parameters, or 0 if n is 0.
+*Return: The sum of all parameters, or 0 if n is 0.
 */
 int sum_them_all(const unsigned int n, ...)
 {
 va_list args;
-int sum = (0);
-unsigned int (i);
+int sum = 0;
+unsigned int i;
 if (n == 0)
 return (0);
 va_start(args, n);
