9fa7eb190a2a2bd0f06efb596957c7b72b3974a0
root <saynezironx@gmail.com>
2024-12-04 12:11:44 +0100

    i test a temporaly code for task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
 /**
  * add_node - Adds a new node at the beginning of a list_t list
  * @head: Pointer to the pointer of the head of the list
  * @str: String to be added to the new node
- *
  * Return: Address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
