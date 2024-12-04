f4f4af959375a69811c7be37e9cbe2ac97eeb339
root <saynezironx@gmail.com>
2024-12-04 13:28:55 +0100

    some probleme with my code

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,28 +1,29 @@
-#include <string.h>
-#include <stdlib.h>
 #include "lists.h"
 /**
-*add_node - Adds a new node at the beginning of a list_t list.
-*@head: A pointer to the pointer of the first node.
-*@str: The string to be added to the new node.
-*Return: The address of the new element, or NULL if it failed.
+* add_node - Adds a new node at the beginning of a list_t list
+* @head: Pointer to the pointer of the first node 
+* @str: String to be added to the new node
+* Return: Address of the new element, or NULL if it failed
 */
-list_t *add_node(list_t **head, const char *str) 
+list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
-char *new_str;
-new_node = malloc(sizeof(list_t));
-if (!new_node)
-return (NULL);
-new_str = strdup(str);
-if (!new_str)
-{
-free(new_node);
-return (NULL);
-}
-new_node->str = new_str;
-new_node->len = strlen(new_str);
-new_node->next = *head;
-*head = new_node;
-return (new_node);
+	list_t *new_node;
+	char *dup_str;
+	unsigned int len = 0;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	while (str[len])
+		len++;
+	new_node->str = dup_str;
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
