98f033ad6be1dfa15fdbf7c226accdb3271c229b
root <saynezironx@gmail.com>
2024-12-04 13:16:00 +0100

    i have done task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
-* add_node - Adds a new node at the beginning of a list_t list.
-* @head: A pointer to the pointer of the first node.
-* @str: The string to be added to the new node.
-* Return: The address of the new element, or NULL if it failed.
+*add_node - Adds a new node at the beginning of a list_t list.
+*@head: A pointer to the pointer of the first node.
+*@str: The string to be added to the new node.
+*Return: The address of the new element, or NULL if it failed.
 */
 list_t *add_node(list_t **head, const char *str) 
 {
