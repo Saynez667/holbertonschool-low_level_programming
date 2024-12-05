237efd4ce809167b9994bcfd93857cf2f1839a91
Saynez667 <saynezironx@gmail.com>
2024-12-05 16:10:32 +0100

    i have change my code

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
-* print_list - prints all the elements of a list_t list
-* @h: head of the list
-* Return: the number of nodes
+*print_list - prints all the elements of a list_t list
+*@h: head of the list
+*Return: the number of nodes
 */
-size_t print_list(const list_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 while (h != NULL)
