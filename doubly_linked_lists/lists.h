cadcde574619972e46a346f45a9a8b65ae9bb2fc
Saynez667 <saynezironx@gmail.com>
2024-12-05 16:04:28 +0100

    i have resolve some probleme with my list.h

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,5 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
 /**
  *struct dlistint_s - doubly linked list
  *@n: integer
@@ -13,9 +15,7 @@ typedef struct dlistint_s
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 }dlistint_t;
-#include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
+
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
