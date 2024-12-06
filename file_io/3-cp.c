1dbde69fe97512e2df996c60bade678e2085ddf2
root <saynezironx@gmail.com>
2024-12-06 15:40:38 +0100

    i want to test a code for task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,54 @@
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[]) {
+  int file_from, file_to;
+  ssize_t bytes_read, bytes_written;
+  char buffer[BUFFER_SIZE];
+
+  // Check if the correct number of arguments is provided
+  if (argc != 3) {
+  dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+  exit(97);
+  }
+
+  // Open the source file for reading
+  file_from = open(argv[1], O_RDONLY);
+  if (file_from == -1) {
+  dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+  exit(98);
+  }
+
+  // Open the destination file for writing, truncate if it already exists
+  file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  if (file_to == -1) {
+  dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+  exit(99);
+  }
+
+  // Copy the content of file_from to file_to
+  while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0) {
+  bytes_written = write(file_to, buffer, bytes_read);
+  if (bytes_written == -1) {
+  dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+  exit(99);
+  }
+  }
+
+  // Check for read error
+  if (bytes_read == -1) {
+  dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+  exit(98);
+  }
+
+  // Close the file descriptors
+  if (close(file_from) == -1) {
+  dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+  exit(100);
+  }
+  if (close(file_to) == -1) {
+  dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+  exit(100);
+  }
+
+  return 0;
+}
